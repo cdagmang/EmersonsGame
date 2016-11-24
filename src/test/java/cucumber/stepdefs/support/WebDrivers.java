dd781bb0ed09925fbb625e677853fa5d907ae8f5
Arcilla <marvin.arcilla@nokia.com>
2016-11-24 14:34:56 +0800

    Added WebDriver again

---
+++ src/test/java/cucumber/stepdefs/support/WebDrivers.java
@@ -0,0 +1,21 @@
+package cucumber.stepdefs.support;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDrivers {
+    private static WebDriver driver;
+
+    public static WebDriver getNewDriver() {
+        driver =  new FirefoxDriver();
+        return driver;
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = getNewDriver();
+        }
+
+        return driver;
+    }
+}
